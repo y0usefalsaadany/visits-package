69f7cceecf49bb9531d36e774f7d3fced248ee6a
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-07-04 14:49:11 +0200

    Add files via upload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'yousefpackage/visits',
         'pretty_version' => '1.0.0+no-version-set',
         'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'yousefpackage/visits',
         'dev' => true,
     ),
     'versions' => array(
         'yousefpackage/visits' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
             'dev_requirement' => false,
         ),
     ),
