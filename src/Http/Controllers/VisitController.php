788c79549136a44b00802cd576fe7de466661c8c
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-07-04 14:59:12 +0200

    Update VisitController.php

--- src/Http/Controllers/VisitController.php
+++ src/Http/Controllers/VisitController.php
@@ -10,14 +10,5 @@ use Illuminate\Support\Facades\DB;
 
 class VisitController extends Controller
 {
-    function countVisit(Request $request){
-        $api = Http::get('http://ipwho.is/154.181.196.80');  
-        $data = json_decode($api, true); 
-        $visitTable =  new Visit;
-        $visitTable->ip = $request->ip();
-        $visitTable->city = $data['city'];
-        $visitTable->save();
-        $countVisits = DB::table('visits')->count();
-        return $countVisits;
-    }
+   
 }
